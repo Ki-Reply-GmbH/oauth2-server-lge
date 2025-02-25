e19a3dad8096f1e30d3e64b91d212ff531ced0be
Lucas Gehlen <l.gehlen@reply.de>
2025-02-25 16:58:41 +0100

    Implemented Basic Auth

--- src/main.go
+++ src/main.go
@@ -1,27 +1,84 @@
 package main
 
 import (
+	"crypto/sha256"
+	"crypto/subtle"
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
 type TokenResponse struct {
 	Token string `json:"Token"`
 }
 
-func main() {
-	http.HandleFunc("/token", getToken)
+type Application struct {
+	auth struct {
+		username string
+		password string
+	}
+}
 
-	// Start server
-	fmt.Println("Starting OAuth2 server on :8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+func main() {
+	app := new(Application)
+
+	app.auth.username = os.Getenv("AUTH_USERNAME")
+	app.auth.password = os.Getenv("AUTH_PASSWORD")
+
+	// Get basic auth username + password from environment
+	if app.auth.username == "" {
+		log.Fatal("basic auth username must be provided")
+	}
+
+	if app.auth.password == "" {
+		log.Fatal("basic auth password must be provided")
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/token", app.basicAuth(app.getToken))
+
+	server := &http.Server{
+		Addr:         ":8080",
+		Handler:      mux,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	// Start server with TLS and local keys at the moment
+	log.Printf("starting OAuth2 server on %s", server.Addr)
+	if err := server.ListenAndServeTLS("../certs/localhost.pem", "../certs/localhost-key.pem"); err != nil {
 		log.Fatalf("Failed to start server: %v", err)
 	}
 }
 
-func getToken(writer http.ResponseWriter, request *http.Request) {
+func (app *Application) basicAuth(next http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		username, password, ok := request.BasicAuth()
+		if ok {
+			usernameHash := sha256.Sum256([]byte(username))
+			passwordHash := sha256.Sum256([]byte(password))
+			expectedUsernameHash := sha256.Sum256([]byte(app.auth.username))
+			expectedPasswordHash := sha256.Sum256([]byte(app.auth.password))
+
+			usernameMatch := (subtle.ConstantTimeCompare(usernameHash[:], expectedUsernameHash[:]) == 1)
+			passwordMatch := (subtle.ConstantTimeCompare(passwordHash[:], expectedPasswordHash[:]) == 1)
+
+			if usernameMatch && passwordMatch {
+				next.ServeHTTP(writer, request)
+				return
+			}
+		}
+
+		// Return unauthorized if Basic Auth fails
+		writer.Header().Set("WWW-Authenticate", `Basic realm="restricted", charset="UTF-8"`)
+		http.Error(writer, "Unauthorized", http.StatusUnauthorized)
+	})
+}
+
+func (app *Application) getToken(writer http.ResponseWriter, request *http.Request) {
 	response := TokenResponse{Token: "xxx"}
 
 	writer.Header().Set("Content-Type", "application/json")
